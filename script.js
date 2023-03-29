8d5948d851c2ca71af217f43727bce4048ccb117
Flowerking666 <susanne@kolloen.no>
2023-03-29 12:34:00 +0200

    Hello

--- script.js
+++ script.js
@@ -1,51 +1,16 @@
 function toggleDiv(divNum) {
     // Hide all divs
-    for (var i = 1; i <= 3; i++) {
-      document.getElementById("div" + i).style.display = "none";
+    for (var i = 1; i <= 11; i++) {
+      document.getElementById("question" + i).style.display = "none";
     }
     // Show the selected div
-    document.getElementById("div" + divNum).style.display = "block";
-  }
-/*var value = 0;
-
-var addFive = document.getElementById("addFive");
-var subFive = document.getElementById("subFive");
-
-var addThree = document.getElementById("addThree");
-var subThree = document.getElementById("subThree");
-
-var addOne = document.getElementById("addOne");
-var subOne = document.getElementById("subOne");
-
-console.log(value)
-
-addFive.onclick = function() {
-    value +=5;
-    console.log(value);
+    document.getElementById("question" + divNum).style.display = "block";
 };
 
-subFive.onclick = function() {
-    value -=5;
-    console.log(value);
-};
+//
+let poeng = 0;
 
-addThree.onclick = function() {
-    value +=3;
-    console.log(value);
-};
-
-subThree.onclick = function() {
-    value -=3;
-    console.log(value);
-};
-
-addOne.onclick = function() {
-    value +=1;
-    console.log(value);
-};
-
-subOne.onclick = function() {
-    value -=1;
-    console.log(value);
-};
-*/
+function addPoints(points) {
+    poeng += points;
+    document.getElementById("poeng").innerHTML = poeng;
+}
